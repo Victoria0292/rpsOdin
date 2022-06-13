1ac6614483a75165523795435971964171b0e328
Victoria0292 <tlynn0292@gmail.com>
2022-06-13 10:21:18 -0400

    defined playerScore and computerScore main.js

--- main.js
+++ main.js
@@ -7,12 +7,19 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
       return `It's a tie! You both chose ${playerSelection}`;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      playerScore++;
       return "WooHoo! You win! Rock beats Scissors";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+      playerScore++;
       return "Aw yeah! You win! Paper beats Rock";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+      playerScore++;
       return "Woooo! You win! Scissors beats Paper";
     } else {
+      computerScore++;
       return `Oh no! You lose! ${computerSelection} beats ${playerSelection}`;
     }
   }
+
+  let playerScore = parseInt(0);
+  let computerScore = parseInt(0);
