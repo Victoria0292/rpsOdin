78dc4ef94c741ffad6be48e30cd28f98533280f2
Victoria0292 <tlynn0292@gmail.com>
2022-06-13 10:18:45 -0400

    added playRound and selections main.js

--- main.js
+++ main.js
@@ -2,3 +2,17 @@ function computerPlay() {
   const choice = ["rock", "paper", "scissors"];
   return choice[Math.floor(Math.random() * choice.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      return `It's a tie! You both chose ${playerSelection}`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      return "WooHoo! You win! Rock beats Scissors";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+      return "Aw yeah! You win! Paper beats Rock";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+      return "Woooo! You win! Scissors beats Paper";
+    } else {
+      return `Oh no! You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+  }
